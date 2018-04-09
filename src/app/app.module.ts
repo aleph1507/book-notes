b93c157e131deb52000267b3953f78c59d587088
aleph1507 <xrristo@gmail.com>
2018-04-09 14:40:59 +0200

    routing0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,45 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
+import { FormsModule } from '@angular/forms';
+import { BooksComponent } from './components/books/books.component';
+import { BookComponent } from './components/book/book.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { EditBookComponent } from './components/edit-book/edit-book.component';
+import { DeleteBookComponent } from './components/delete-book/delete-book.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
+//Routes
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'books', component: BooksComponent },
+  { path: 'book/:id', component: BookComponent },
+  { path: 'add-book', component: AddBookComponent },
+  { path: 'edit-book/:id', component: EditBookComponent },
+  { path: 'delete-book/:id', component: DeleteBookComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BooksComponent,
+    BookComponent,
+    AddBookComponent,
+    EditBookComponent,
+    DeleteBookComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
